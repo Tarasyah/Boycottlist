ef10f0b92f1141c075682b7299141248327f1add
Skat Skadi <skatskadi17@gmail.com>
2025-08-05 07:17:15 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <TableHead classNa

--- src/components/conscience-cart/boycott-list.tsx
+++ src/components/conscience-cart/boycott-list.tsx
@@ -63,7 +63,7 @@ export function BoycottList({ companies }: { companies: Company[] }) {
       accessorKey: "Involvement",
       header: () => <div className="text-center">Involvement</div>,
       cell: ({ row }) => (
-        <div className="text-muted-foreground max-w-sm sm:max-w-lg md:max-w-3xl p-1 text-xs sm:text-sm sm:p-2 md:p-4 text-center">
+        <div className="text-muted-foreground max-w-sm sm:max-w-md md:max-w-2xl p-1 text-xs sm:text-sm sm:p-2 md:p-4 text-center">
           {row.getValue("Involvement")}
         </div>
       ),
